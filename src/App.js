9a0b01b0a25e8fd9317a014ef196ce694d58a8ce
jongkyuu <minerva421.ai@gmail.com>
2023-07-14 03:30:19 +0900

    CreateUser 추가

--- src/App.js
+++ src/App.js
@@ -8,11 +8,96 @@ import {
     TableCell,
     TableBody,
 } from "@mui/material";
+import React, { useState, useRef } from "react";
+import CreateUser from "./CreateUser";
 
 function App() {
+    useState();
+
+    const nextId = useRef(4);
+
+    const onChange = (e) => {
+        const { name, value } = e.target;
+        setInputs({
+            ...inputs,
+            [name]: value,
+        });
+    };
+
+    const onCreate = () => {
+        console.log(`nextID : ${nextId.current}`);
+        const user = {
+            id: nextId.current,
+            image: `https://picsum.photos/id/${nextId.current}/64/64`,
+            name,
+            birthday,
+            gender,
+            job,
+        };
+
+        console.log(user);
+
+        setUsers([...users, user]);
+
+        setInputs({
+            name: "",
+            birthday: "",
+            gender: "",
+            job: "",
+        });
+
+        nextId.current += 1;
+    };
+
+    const [inputs, setInputs] = useState({
+        name: "",
+        birthday: "",
+        gender: "",
+        job: "",
+    });
+
+    const { name, birthday, gender, job } = inputs;
+
+    const [users, setUsers] = useState([
+        {
+            id: 1,
+            image: "https://picsum.photos/id/1/64/64",
+            name: "John Doe",
+            birthday: "1985-07-15",
+            gender: "Male",
+            job: "Software Engineer",
+        },
+        {
+            id: 2,
+            image: "https://picsum.photos/id/2/64/64",
+            name: "Jane Smith",
+            birthday: "1990-03-22",
+            gender: "Female",
+            job: "Marketing Manager",
+        },
+        {
+            id: 3,
+            image: "https://picsum.photos/id/3/64/64",
+            name: "Alex Johnson",
+            birthday: "1978-11-05",
+            gender: "Male",
+            job: "Teacher",
+        },
+    ]);
+
     return (
         <div className="App">
             <h1>User Table</h1>
+
+            <CreateUser
+                name={name}
+                birthday={birthday}
+                gender={gender}
+                job={job}
+                onChange={onChange}
+                onCreate={onCreate}
+            />
+
             <Table>
                 <TableHead>
                     <TableRow>
@@ -39,32 +124,5 @@ function App() {
         </div>
     );
 }
-
-let users = [
-    {
-        id: 1,
-        image: "https://picsum.photos/id/1/64/64",
-        name: "John Doe",
-        birthday: "1985-07-15",
-        gender: "Male",
-        job: "Software Engineer",
-    },
-    {
-        id: 2,
-        image: "https://picsum.photos/id/2/64/64",
-        name: "Jane Smith",
-        birthday: "1990-03-22",
-        gender: "Female",
-        job: "Marketing Manager",
-    },
-    {
-        id: 3,
-        image: "https://picsum.photos/id/3/64/64",
-        name: "Alex Johnson",
-        birthday: "1978-11-05",
-        gender: "Male",
-        job: "Teacher",
-    },
-];
 
 export default App;
---
+++ src/CreateUser.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+function CreateUser({ name, birthday, gender, job, onChange, onCreate }) {
+    return (
+        <div>
+            <input
+                name="name"
+                placeholder="name"
+                onChange={onChange}
+                value={name}
+            />
+            <input
+                name="birthday"
+                placeholder="birthday"
+                onChange={onChange}
+                value={birthday}
+            />
+            <input
+                name="gender"
+                placeholder="gender"
+                onChange={onChange}
+                value={gender}
+            />
+            <input
+                name="job"
+                placeholder="job"
+                onChange={onChange}
+                value={job}
+            />
+            <button onClick={onCreate}>등록</button>
+        </div>
+    );
+}
+
+export default CreateUser;
