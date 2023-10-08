c85b53b45e6a379148c09c28d11ef29c6c19ba10
minerva421 <minerva421.ai@gmail.com>
2023-10-09 01:13:38 +0900

    useInputs를 useState에서 useReducer 사용하도록 변경

--- src/useInputs.js
+++ src/useInputs.js
@@ -1,18 +1,44 @@
-import { useState, useCallback} from 'react';
+import { useState, useCallback, useReducer} from 'react';
 
 function reducer(state, action){
     // CHANGE
     // RESET
+    switch (action.type) {
+        case "CHANGE":
+            return {
+                ...state,
+                [action.name] : action.value
+            };
+        case "RESET":
+            // return action.initialForm;
+            return Object.keys(state).reduce((acc, current) => {
+                acc[current] = '';
+                return acc;
+            }, {});
+        default:
+            throw new Error("Unhandled action");
+    }
 }
 
 function useInputs(initialForm) {
     // Reducer 사용하도록 변경해보기!
-    const [form, setForm] = useState(initialForm);
+    // const [form, setForm] = useState(initialForm);
+    const [form, dispatch] = useReducer(reducer, initialForm);
+
     const onChange = useCallback(e => {
         const { name, value } = e.target;
-        setForm(form => ({ ...form, [name]: value}));
+        // setForm(form => ({ ...form, [name]: value}));
+        dispatch({
+            type: "CHANGE",
+            name,
+            value
+        })
     }, []);
-    const reset = useCallback(() => setForm(initialForm), [initialForm]);
+    // const reset = useCallback(() => setForm(initialForm), [initialForm]);
+    const reset = useCallback(() => dispatch({
+        type:"RESET",
+        // initialForm
+    }), [])
 
     return [form, onChange, reset];
 };
