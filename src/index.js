ee64ceccf93b240ac47a4c29799ee904b58e8c02
minerva421 <minerva421.ai@gmail.com>
2023-10-09 23:56:15 +0900

    ContextAPI 사용

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 // import Counter from "./Counter";
+import ContextSample from "./ContextSample";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <App />
+        <ContextSample />
     </React.StrictMode>
 );
 
